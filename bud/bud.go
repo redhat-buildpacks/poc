1265c9acc9008e8e2df6fa21cc1b1d7c4daa614b
Charles Moulliard <cmoulliard@redhat.com>
2021-10-28 16:23:05 +0200

    Add the missing parameter %s to well format the code. Removethe code non needed

--- bud/bud.go
+++ bud/bud.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/containers/buildah"
 	"github.com/containers/buildah/imagebuildah"
-	is "github.com/containers/image/v5/storage"
 	"github.com/containers/storage"
 	"github.com/containers/storage/pkg/unshare"
 	"github.com/sirupsen/logrus"
@@ -26,10 +24,10 @@ func main() {
 	b := util.InitOptions()
 
 	os.Setenv("BUILDAH_TEMP_DIR", b.TempDir)
-	logrus.Infof("Buildah tempdir : ",b.TempDir)
+	logrus.Infof("Buildah tempdir: %s",b.TempDir)
 
 	dockerFileName := filepath.Join(b.WorkspaceDir, "Dockerfile")
-	logrus.Infof("Dockerfile : ",dockerFileName)
+	logrus.Infof("Dockerfile: %s",dockerFileName)
 
 	// storeOptions, err := storage.DefaultStoreOptions(false,0)
 
@@ -41,21 +39,8 @@ func main() {
 
 	imageID, _, err := imagebuildah.BuildDockerfiles(ctx, store, b.BuildOptions, dockerFileName)
 	if err != nil {
-		logrus.Errorf("Build image failed: ",err.Error())
+		logrus.Errorf("Build image failed: %s",err.Error())
 	}
-	logrus.Infof("Image id : ",imageID)
-
-	builder, err := buildah.ImportBuilderFromImage(context.TODO(), store,buildah.ImportFromImageOptions{Image: imageID})
-	if err != nil {
-		panic(err)
-	}
-
-	imageRef, err := is.Transport.ParseStoreReference(store, imageID)
-	if err != nil {
-		logrus.Errorf( "no such image %q", imageID)
-	}
-
-	imageId, _, _, err := builder.Commit(context.TODO(), imageRef, buildah.CommitOptions{})
-
-	fmt.Printf("Image built! %s\n", imageId)
+	logrus.Infof("Image id: %s",imageID)
+	logrus.Info("Image built successfully :-)" )
 }
