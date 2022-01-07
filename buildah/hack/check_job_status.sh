a8c1eab97887369a7c3765bd8292c354544c5ac9
Charles Moulliard <cmoulliard@redhat.com>
2022-01-07 17:31:57 +0100

    Create a script to check the job status (complete or failed)

---
+++ buildah/hack/check_job_status.sh
@@ -0,0 +1,20 @@
+while true; do
+  if kubectl -n buildpack wait --timeout=-1s --for=condition=complete job/buildah-poc 2>/dev/null; then
+    job_result=0
+    break
+  fi
+
+  if kubectl -n buildpack wait --timeout=-1s --for=condition=failed job/buildah-poc 2>/dev/null; then
+    job_result=1
+    break
+  fi
+
+  sleep 3
+done
+
+if [[ $job_result -eq 1 ]]; then
+    echo "Job failed!"
+    exit 1
+fi
+
+echo "Job succeeded"
