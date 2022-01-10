fad5e18bd696b3f4f97caee674625ddd8100c664
Charles Moulliard <cmoulliard@redhat.com>
2022-01-10 17:24:40 +0100

    Create new job to test kaniko. Update the github flow

---
+++ kaniko/hack/check_job_status.sh
@@ -0,0 +1,21 @@
+while true; do
+  if kubectl -n buildpack wait --timeout=300s --for=condition=complete job/kaniko-poc 2>/dev/null; then
+    job_result=0
+    break
+  fi
+
+  if kubectl -n buildpack wait --timeout=300s --for=condition=failed job/kaniko-poc 2>/dev/null; then
+    job_result=1
+    break
+  fi
+
+  sleep 3
+done
+
+if [[ $job_result -eq 1 ]]; then
+    echo "Job failed!"
+    exit 1
+fi
+
+echo "Job succeeded"
+exit 0
