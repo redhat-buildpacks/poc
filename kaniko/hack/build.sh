a38525d9c0220e82477f5cdfe7eb38e676fee695
Charles Moulliard <cmoulliard@redhat.com>
2022-01-11 11:53:55 +0100

    #gen. Continue the tests. Add logic to build the go POC application

--- kaniko/hack/build.sh
+++ kaniko/hack/build.sh
@@ -3,7 +3,14 @@
 set -e
 
 pushd code
+echo "### Remove vendor folder ..."
+rm -rf vendor
+echo "### Go mod vendor ..."
+go mod vendor
+
+echo "### Compiling POC app ..."
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -gcflags "all=-N -l" -o out/kaniko-app main.go
 popd
 
+echo "### Creating the POC image ..."
 docker build -t kaniko-app -f Dockerfile_build .
